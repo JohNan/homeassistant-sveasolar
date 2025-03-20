63d7a7908cea792fc361472870e7e14378979946
Johan Nenzén <johan.nanzen@gmail.com>
2025-03-20 13:21:34 +0100

    Initial commit

---
+++ custom_components/sveasolar/config_flow.py
@@ -0,0 +1,38 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+
+from .const import DOMAIN
+
+class SveaSolarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Svea Solar", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("username"): str,
+                vol.Required("password"): str,
+            })
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return SveaSolarOptionsFlowHandler(config_entry)
+
+class SveaSolarOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({})
+        )
